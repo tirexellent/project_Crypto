d4d11692c9146d48cbad81a65a0c576740966e47
NathanAnto <nathan4567@oulook.com>
2024-02-24 12:26:59 +0100

    Sending and recieving data

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import codecs
 import socket
 
 hostname=socket.gethostname()
@@ -9,47 +10,54 @@ SERVER_PORT = 6000
 
 
 
-# OPENING LISTENING PORT
-# AF_INET = IPv4, SOCK_STREAM = TCP
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-
-#     conn, addr = s.accept()
-#     with conn:
-#         print(f"Connected by {addr}")
-#         while (true):
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             print(f"Data recieved: {data}")
+# returns length in 2 bytes
+def get_message_length(length):
+    res = length.to_bytes(2, byteorder='big')
+    return res
 
 
-def create_message(m):
+# returns message as 4 bytes per character
+def get_message_characters(message):
+    res = bytearray()
+    if isinstance(message, str):
+        for c in message:
+            res.extend(c.encode().rjust(4, b'\x00'))
+    return res
+
+
+def create_message(m, t):
     data = []
-    if isinstance(m, str):
-        length = len(m)
-        
-        data = b"ISCt"
-        data += str(length).encode()
+    if isinstance(m, str):        
+        data = b"ISC"
+        data += t.encode()
 
-        for i in range(length):
-            data += m[i].encode()
-        
-        print(data)
+        if t == 't':
+            length = len(m)
+            # 2 bytes for length
+            data += get_message_length(length)
+            # 4 bytes per character in message
+            data += get_message_characters(m)
+        elif t == 'i':
+            print("image")
+        elif t == 's':
+            print("server")
+
+        print(f"Sent data: {data}")
 
     return data
 
-# CLIENT
+# OPENING LISTENING PORT
+# AF_INET = IPv4, SOCK_STREAM = TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((SERVER_HOST, SERVER_PORT))
 
-    message = "g"
+    message = "test"
+    type = "t"
 
-    data = create_message(message)
+    data = create_message(message, type)
 
     s.sendall(data)
     rcv = s.recv(1024)
     
-print(f"Received {data!r}")
+print(f"Received {rcv.decode()}")
